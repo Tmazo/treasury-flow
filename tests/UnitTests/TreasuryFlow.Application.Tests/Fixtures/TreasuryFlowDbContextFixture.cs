3a90a9f5424aa07105f3acb08188f50d8ca09496
Thiago Mazo <thiago.mazo@hotmail.com>
2026-01-26 20:51:46 -0300

    test: change tests and implement fixture..

---
+++ tests/UnitTests/TreasuryFlow.Application.Tests/Fixtures/TreasuryFlowDbContextFixture.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TreasuryFlow.Infrastructure.Shared.Data;
+
+namespace TreasuryFlow.Application.Tests.Fixtures;
+
+public class TreasuryFlowDbContextFixture : IDisposable
+{
+    public TreasuryFlowDbContext DbContext { get; }
+
+    public TreasuryFlowDbContextFixture()
+    {
+        var options = new DbContextOptionsBuilder<TreasuryFlowDbContext>()
+            .UseInMemoryDatabase($"TreasuryFlow-Test-{Guid.NewGuid()}")
+            .Options;
+
+        DbContext = new TreasuryFlowDbContext(options);
+    }
+
+    public void Dispose()
+    {
+        DbContext.Dispose();
+    }
+}
--- tests/UnitTests/TreasuryFlow.Application.Tests/TransactionsTests/ServicesTests/TransactionServiceTests/CreateAsyncTests.cs
+++ tests/UnitTests/TreasuryFlow.Application.Tests/TransactionsTests/ServicesTests/TransactionServiceTests/CreateAsyncTests.cs
@@ -2,34 +2,36 @@
 using Microsoft.EntityFrameworkCore;
 using NSubstitute;
 using TreasuryFlow.Application.Shared.Communications.Interfaces;
-using TreasuryFlow.Application.Shared.Data.Interfaces;
 using TreasuryFlow.Application.Shared.Exceptions;
+using TreasuryFlow.Application.Tests.Fixtures;
 using TreasuryFlow.Application.Transactions.Inputs;
 using TreasuryFlow.Application.Transactions.Services;
-using TreasuryFlow.Domain.Transactions.Entities;
 using TreasuryFlow.Domain.Transactions.Enums;
 using TreasuryFlow.Domain.User.Entities;
+using TreasuryFlow.Infrastructure.Shared.Data;
 
 namespace TreasuryFlow.Application.Tests.TransactionsTests.ServicesTests.TransactionServiceTests;
 
-public class CreateAsyncTests
+public class CreateAsyncTests : IClassFixture<TreasuryFlowDbContextFixture>
 {
+    private readonly TreasuryFlowDbContext _context;
+
+    public CreateAsyncTests(TreasuryFlowDbContextFixture fixture) => 
+        _context = fixture.DbContext;
+
     [Fact]
-    public async Task CreateAsync_WhenUserDoesNotExist_ShouldThrowForbiddenAccessException()
+    public async Task CreateAsync_WhenUserDoesNotExist_ShouldThrowForbidden()
     {
         // Arrange
-        var context = Substitute.For<ITreasuryFlowDbContext>();
-
-        context.Users.Returns(
-            new List<UserEntity>().AsQueryable());
-
         var publisher = Substitute.For<IEventPublisher>();
 
-        var service = new TransactionService(context, publisher);
+        var service = new TransactionService(_context, publisher);
+
+        var input = new CreateTransactionInput(Guid.NewGuid(), 100, ETransactionType.Input);
 
         // Act
         Func<Task> act = () =>
-            service.CreateAsync(Arg.Any<CreateTransactionInput>(), CancellationToken.None);
+            service.CreateAsync(input, CancellationToken.None);
 
         // Assert
         await act.Should()
@@ -41,29 +43,22 @@ public class CreateAsyncTests
     public async Task CreateAsync_WhenUserExists_ShouldPersistAndPublish()
     {
         // Arrange
-        var userId = new Guid();
+        var user = new UserEntity
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test",
+            Email = "test@test.com",
+            PasswordHash = "545ccaddc"
+        };
 
-        var context = Substitute.For<ITreasuryFlowDbContext>();
-
-        context.Users.Returns(
-            new List<UserEntity>()
-            { 
-                new()
-                { 
-                    Id = userId,
-                    Name = "test",
-                    Email = "test@test.com",
-                    PasswordHash = "d7svfvs4vf84f"
-                }
-            }.AsQueryable());
-
-        context.Transactions.Returns(Substitute.For<DbSet<TransactionEntity>>());
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
 
         var publisher = Substitute.For<IEventPublisher>();
 
-        var service = new TransactionService(context, publisher);
+        var service = new TransactionService(_context, publisher);
 
-        var input = new CreateTransactionInput(userId, 10, ETransactionType.Input);
+        var input = new CreateTransactionInput(user.Id, 100, ETransactionType.Input);
 
         // Act
         var result = await service.CreateAsync(
@@ -73,11 +68,8 @@ public class CreateAsyncTests
         // Assert
         result.TransactionId.Should().NotBeEmpty();
 
-        await context.Transactions.Received(1)
-            .AddAsync(Arg.Any<TransactionEntity>(), Arg.Any<CancellationToken>());
-
-        await context.Received(1)
-            .SaveChangesAsync(Arg.Any<CancellationToken>());
+        (await _context.Transactions.CountAsync())
+            .Should().Be(1);
 
         await publisher.Received(1)
             .SendAsRawJsonAsync(

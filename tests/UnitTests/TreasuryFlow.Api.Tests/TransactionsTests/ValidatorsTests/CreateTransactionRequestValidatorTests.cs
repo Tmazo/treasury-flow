d8e2641e872f022c0f12e4502c5f9de0c7aa6ab1
Thiago Mazo <thiago.mazo@hotmail.com>
2026-01-29 12:33:35 -0300

    test: create validator tests.

---
+++ tests/UnitTests/TreasuryFlow.Api.Tests/TransactionsTests/ValidatorsTests/CreateTransactionRequestValidatorTests.cs
@@ -0,0 +1,69 @@
+﻿using FluentValidation.TestHelper;
+using TreasuryFlow.Api.Transactions.Requests;
+using TreasuryFlow.Api.Transactions.Validators;
+using TreasuryFlow.Domain.Transactions.Enums;
+
+namespace TreasuryFlow.Api.Tests.Transactions.Validators;
+
+public class CreateTransactionRequestValidatorTests
+{
+    readonly CreateTransactionRequestValidator _validator;
+
+    public CreateTransactionRequestValidatorTests()
+    {
+        _validator = new CreateTransactionRequestValidator();
+    }
+
+    [Fact]
+    public void Should_have_error_when_amount_is_zero_or_negative()
+    {
+        // Arrange
+        var request = new CreateTransactionRequest
+        {
+            Amount = 0,
+            TransactionType = ETransactionType.Input
+        };
+
+        // Act
+        var result = _validator.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Amount)
+              .WithErrorMessage("Amount must be greater than zero.");
+    }
+
+    [Fact]
+    public void Should_have_error_when_transaction_type_is_invalid()
+    {
+        // Arrange
+        var request = new CreateTransactionRequest
+        {
+            Amount = 100,
+            TransactionType = (ETransactionType)999
+        };
+
+        // Act
+        var result = _validator.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.TransactionType)
+              .WithErrorMessage("Type must be a valid transaction type.");
+    }
+
+    [Fact]
+    public void Should_not_have_error_when_request_is_valid()
+    {
+        // Arrange
+        var request = new CreateTransactionRequest
+        {
+            Amount = 150,
+            TransactionType = ETransactionType.Input
+        };
+
+        // Act
+        var result = _validator.TestValidate(request);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
---
+++ tests/UnitTests/TreasuryFlow.Api.Tests/UserBalancesTests/ValidatorsTests/GetUserBalanceByPeriodRequestValidatorTests.cs
@@ -0,0 +1,86 @@
+﻿using FluentValidation.TestHelper;
+using TreasuryFlow.Api.UserBalances.Requests;
+using TreasuryFlow.Api.UserBalances.Validators;
+
+namespace TreasuryFlow.Api.Tests.UserBalancesTests.ValidatorsTests;
+
+public class GetUserBalanceByPeriodRequestValidatorTests
+{
+    readonly GetUserBalanceByPeriodRequestValidator _validator;
+
+    public GetUserBalanceByPeriodRequestValidatorTests()
+    {
+        _validator = new GetUserBalanceByPeriodRequestValidator();
+    }
+
+    [Fact]
+    public void Should_have_error_when_initial_period_is_empty()
+    {
+        // Arrange
+        var request = new GetUserBalanceByPeriodRequest
+        {
+            InitialPeriod = default,
+            FinalPeriod = new DateOnly(2024, 10, 10)
+        };
+
+        // Act
+        var result = _validator.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.InitialPeriod)
+              .WithErrorMessage("InitialDate is required.");
+    }
+
+    [Fact]
+    public void Should_have_error_when_final_period_is_empty()
+    {
+        // Arrange
+        var request = new GetUserBalanceByPeriodRequest
+        {
+            InitialPeriod = new DateOnly(2024, 10, 1),
+            FinalPeriod = default
+        };
+
+        // Act
+        var result = _validator.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.FinalPeriod)
+              .WithErrorMessage("FinalDate is required.");
+    }
+
+    [Fact]
+    public void Should_have_error_when_initial_period_is_greater_than_final_period()
+    {
+        // Arrange
+        var request = new GetUserBalanceByPeriodRequest
+        {
+            InitialPeriod = new DateOnly(2024, 10, 10),
+            FinalPeriod = new DateOnly(2024, 10, 1)
+        };
+
+        // Act
+        var result = _validator.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x)
+              .WithErrorMessage("InitialDate must be less than or equal to FinalDate.");
+    }
+
+    [Fact]
+    public void Should_not_have_error_when_period_is_valid()
+    {
+        // Arrange
+        var request = new GetUserBalanceByPeriodRequest
+        {
+            InitialPeriod = new DateOnly(2024, 10, 1),
+            FinalPeriod = new DateOnly(2024, 10, 31)
+        };
+
+        // Act
+        var result = _validator.TestValidate(request);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
